f945f8a1200724a863c97e49079f220b06e6dcc7
irodakarimov <irodakarimova896@gmail.com>
2024-12-15 19:31:53 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function lightmode() {
+    const li1 = document.getElementById("li1");
+    document.body.style.backgroundColor = "#C4B5A5";
+    document.body.style.color = "black";
+}
+
+
+
+function darkmode() {
+    const li = document.getElementById("li");
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
+}
+
